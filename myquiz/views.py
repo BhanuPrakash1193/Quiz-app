bf538f55bb1e4e3e8bd96849f6af087baa6121a6
BhanuPrakash1193 <93832950+BhanuPrakash1193@users.noreply.github.com>
2022-02-01 12:37:07 +0530

    Add files via upload

---
+++ myquiz/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def index(request):
+	return render(request, 'index.html')
+
+def questions(request):
+	return render(request, 'quiz.html')
+
+def questionsV(request):
+	return render(request, 'quizV.html')
+
+def questionsA(request):
+	return render(request, 'quizA.html')
+
+def login(request):
+	return render(request, 'username.html')
+
